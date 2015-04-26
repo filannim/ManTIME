005724c1bf7172472f1b36175c12fa590968fc0c
Michele Filannino <filannino.m@gmail.com>
2015-04-26 12:35:57 +0100

    argparse is cool

--- mantime/mantime.py
+++ mantime/mantime.py
@@ -17,20 +17,16 @@ __version__ = "0.1"
 __codename__ = "purple tempo"
 
 import argparse
-import glob
-import os
+import codecs
 import cPickle
+import glob
 import logging
-import sys
+import os
 import xml.etree.cElementTree as cElementTree
 
-from readers import i2b2FileReader
 from readers import TempEval3FileReader
-from readers import WikiWarsInLineFileReader
-from attributes_extractor import FullExtractor
 from writers import TempEval3Writer
-from writers import AttributeMatrixWriter
-from writers import i2b2Writer
+from attributes_extractor import FullExtractor
 from classifier import IdentificationClassifier
 from classifier import NormalisationClassifier
 from settings import PATH_MODEL_FOLDER
@@ -68,7 +64,8 @@ class ManTIME(object):
                 doc = self.extractor.extract(self.reader.parse(input_file))
                 self.documents.append(doc)
             except cElementTree.ParseError:
-                msg = 'Document {} skipped.'.format(os.path.relpath(input_file))
+                msg = 'Document {} skipped.'.format(
+                    os.path.relpath(input_file))
                 logging.error(msg)
 
         # storing the attribute extractors caches
@@ -95,7 +92,7 @@ class ManTIME(object):
         # try:
         doc = self.extractor.extract(self.reader.parse(file_name))
         annotated_doc = identifier.test([doc], self.model,
-                                         self.post_processing_pipeline)
+                                        self.post_processing_pipeline)
         annotated_doc = normaliser.test([doc], self.model)
 
         output = self.writer.write(annotated_doc)
@@ -107,13 +104,14 @@ class ManTIME(object):
 def main():
     """ It annotates documents in a specific folder.
     """
-    import codecs
     logging.basicConfig(format='%(asctime)s: %(message)s',
                         level=logging.INFO,
                         datefmt='%m/%d/%Y %I:%M:%S %p')
     # Parse input
     parser = argparse.ArgumentParser(
         description='ManTIME: temporal information extraction')
+    parser.add_argument('mode', choices=['train', 'test'],
+                        help='Train or Test mode?')
     parser.add_argument('input_folder', help='Input data folder path')
     parser.add_argument('model',
                         help='Name of the model to use (case sensitive)')
@@ -124,25 +122,31 @@ def main():
                         help='it uses the post processing pipeline.')
     args = parser.parse_args()
 
-    # Expected usage of ManTIME
+    # ManTIME
     mantime = ManTIME(reader=TempEval3FileReader(),
                       writer=TempEval3Writer(),
                       extractor=FullExtractor(),
                       model_name=args.model,
                       pipeline=False)
-    mantime.train(args.input_folder)
-    # assert os.path.exists(args.input_folder), 'Model not found.'
-    # documents = sorted(glob.glob(os.path.join(args.input_folder, '*.tml')))
-    # assert documents, 'Input folder is empty.'
-    # for doc in documents:
-    #     basename = os.path.basename(doc)
-    #     writein = os.path.join('./output/', basename)
-    #     if not os.path.exists(writein):
-    #         with codecs.open(writein, 'w', encoding='utf8') as output:
-    #             # try:
-    #             output.write(mantime.label(doc)[0])
-    #             # except:
-    #             #    print 'Doc {} skipped.'.format(basename)
+
+    if args.mode == 'train':
+        # Training
+        mantime.train(args.input_folder)
+    else:
+        # Testing
+        assert os.path.exists(args.input_folder), 'Model not found.'
+        input_files = os.path.join(args.input_folder, '*.tml.TE3input')
+        documents = sorted(glob.glob(input_files))
+        assert documents, 'Input folder is empty.'
+        for doc in documents:
+            basename = os.path.basename(doc)
+            writein = os.path.join('./output/', basename)
+            if not os.path.exists(writein):
+                with codecs.open(writein, 'w', encoding='utf8') as output:
+                    # try:
+                    output.write(mantime.label(doc)[0])
+                    # except:
+                    #    print 'Doc {} skipped.'.format(basename)
 
 if __name__ == '__main__':
     main()
